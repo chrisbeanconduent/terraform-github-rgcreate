c33c7490d5fc4990daf100d44bc7ad5d13502bdd
chrisbeanconduent <chris.bean@conduent.com>
2023-05-03 13:06:56 -0400

    initial commit

---
+++ vars.tf
@@ -0,0 +1,26 @@
+variable environment {
+    type = string
+}
+variable tenant_id {
+    type = string
+}
+
+variable subscription_id {
+    type = string
+}
+
+variable client_id {
+    type = string
+}
+
+variable client_secret {
+    type = string
+}
+
+variable name {
+    type = string
+}
+
+variable location {
+    type = string
+}
